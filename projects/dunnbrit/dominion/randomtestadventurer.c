f51ee41e4c287b8a6c2ab98ae71d5269718dce6d
Brittany Dunn <dunnbrit@oregonstate.edu>
2019-03-02 09:02:50 -0800

    Random Tests on Cards

--- projects/dunnbrit/dominion/randomtestadventurer.c
+++ projects/dunnbrit/dominion/randomtestadventurer.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(){
+int main(){ 
 
 	//Setup variables
 	int kingdomCards[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
